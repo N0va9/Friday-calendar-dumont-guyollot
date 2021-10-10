3d1516a54f5eea591043a07c2bdfec6d8b5243f9
thomas dumont <thomas2dumont@gmail.com>
2021-10-10 11:48:07 +0200

    added all EventsAdministrator's class structure and Event interface

---
+++ friday-back/src/main/java/EventsIcalendarAdministrator.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class EventsIcalendarAdministrator {
+    public static void updateEvent(List<Event> event){
+        //TODO
+    }
+
+    public static void removeEvent(Event event){
+        //TODO
+    }
+}
---
+++ friday-back/src/main/java/EventsPersonalAdministrator.java
@@ -0,0 +1,13 @@
+public class EventsPersonalAdministrator {
+    public static void addEvent(Event event){
+        //TODO
+    }
+
+    public static void updateEvent(Event event){
+        //TODO
+    }
+
+    public static void removeEvent(Event event){
+        //TODO
+    }
+}
