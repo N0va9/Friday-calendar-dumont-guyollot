c33b7f70b76b4dfcb8a4e82ac788fff4c556e0d1
thomas dumont <thomas2dumont@gmail.com>
2021-12-27 11:41:31 +0100

    removed old version

--- friday-back-old/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import java.sql.SQLException;
-
-public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        DatabaseCreator.createDataBase("./database", "admin", "admin");
-    }
-}
